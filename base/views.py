8d49695d8228e4e6065472bbcfbac2dc623cbc5d
Mohammedvaraliya <mohammedvaraliya2661392@gmail.com>
2023-04-27 20:28:59 +0530

    Added templates for base app

--- base/views.py
+++ base/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def rooms(request):
+    return render(request, 'room.html')
