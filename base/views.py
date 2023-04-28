d6ab8ec130133f2b04f933c5d15abe84bbda7137
Mohammedvaraliya <mohammedvaraliya2661392@gmail.com>
2023-04-28 18:54:42 +0530

    Add model Room

--- base/views.py
+++ base/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
-rooms = [
-    {'id': 1, 'name':'Lets learn python!'},
-    {'id': 2, 'name':'Design with me'},
-    {'id': 3, 'name':'Frontend developers'},
-]
+rooms = Room.objects.all()
 
 def home(request):
     context = {
@@ -14,11 +11,7 @@ def home(request):
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-    
+    room = Room.objects.get(id=pk)
     context = {
         'room': room
     }
