f1c67b6089800ea925bedc979bc1b714e967051c
Mohammedvaraliya <mohammedvaraliya2661392@gmail.com>
2023-04-28 11:41:01 +0530

    added dynamic urls to the room pattern

--- base/views.py
+++ base/views.py
@@ -8,7 +8,18 @@ rooms = [
 ]
 
 def home(request):
-    return render(request, 'home.html', {'rooms': rooms})
+    context = {
+        'rooms' : rooms
+    }
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    
+    context = {
+        'room': room
+    }
+    return render(request, 'base/room.html', context)
