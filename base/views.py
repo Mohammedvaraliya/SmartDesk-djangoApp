bb5cc3b144abd557ae6656896d773b178e2439aa
Mohammedvaraliya <mohammedvaraliya2661392@gmail.com>
2023-04-29 18:35:23 +0530

    Update search functionality in the project

--- base/views.py
+++ base/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
     topics = Topic.objects.all()
+    room_count = rooms.count()
     context = {
         'rooms' : rooms,
-        'topics' : topics
+        'topics' : topics,
+        'room_count' : room_count
     }
     return render(request, 'base/home.html', context)
 
