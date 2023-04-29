c978d8f7faa82d38907bf54317f5de5bea6c213a
Mohammedvaraliya <mohammedvaraliya2661392@gmail.com>
2023-04-29 15:16:05 +0530

    created a searchbar

--- base/views.py
+++ base/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
 def home(request):
-    rooms = Room.objects.all()
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(topic__name__icontains=q)
+    topics = Topic.objects.all()
     context = {
-        'rooms' : rooms
+        'rooms' : rooms,
+        'topics' : topics
     }
     return render(request, 'base/home.html', context)
 
