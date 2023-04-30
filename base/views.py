6066111640c86e45145d5edbf35571feb60e98d3
Mohammedvaraliya <mohammedvaraliya2661392@gmail.com>
2023-04-30 16:08:28 +0530

    Added registratio authenticatio to the application

--- base/views.py
+++ base/views.py
@@ -6,16 +6,17 @@ from .models import Room, Topic
 from .forms import RoomForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
 # Create your views here.
 def loginPage(request):
-
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
 
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -31,13 +32,37 @@ def loginPage(request):
         else:
             messages.error(request, "Username OR password does not exist.")
 
-    context = {}
+    context = {
+        'page': page
+    }
     return render(request, 'base/login_register.html', context)
 
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+
+def registerPage(request):
+    page = 'register'
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user=user)
+            return redirect('home')
+        else:
+            messages.error(request, "An error occured during registration.")
+
+    context = {
+        'page': page,
+        'form': form
+    }
+    return render(request, 'base/login_register.html', context)
 
 
 def home(request):
