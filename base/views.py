2698f71e71a21513c193606a1b91d901b706ba53
Mohammedvaraliya <mohammedvaraliya2661392@gmail.com>
2023-05-01 20:05:50 +0530

    Added comment section in the room

--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
@@ -83,7 +83,15 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    room_messages = room.message_set.all().order_by('-created') # to get the children foriegn attribute from parent model
+    room_messages = room.message_set.all().order_by('-created') # to get the children foriegn field from parent model
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        return redirect('room', pk=room.id)
     context = {
         'room': room,
         'room_messages': room_messages
