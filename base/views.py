a061a78466cb8ea8505708fbacac32a80a1a460e
Mohammedvaraliya <mohammedvaraliya2661392@gmail.com>
2023-05-02 12:23:19 +0530

    Added participants functionality in messaging the room

--- base/views.py
+++ base/views.py
@@ -85,16 +85,19 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created') # to get the children foriegn field from parent model
 
+    participants = room.participants.all()
     if request.method == 'POST':
         message = Message.objects.create(
             user = request.user,
             room = room,
             body = request.POST.get('body')
         )
+        room.participants.add(request.user)
         return redirect('room', pk=room.id)
     context = {
         'room': room,
-        'room_messages': room_messages
+        'room_messages': room_messages,
+        'participants': participants
     }
     return render(request, 'base/room.html', context)
 
