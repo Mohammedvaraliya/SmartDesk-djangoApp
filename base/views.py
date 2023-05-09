006acc2390cbc3f9974eb05a6cd450ed2652fda9
Mohammedvaraliya <mohammedvaraliya2661392@gmail.com>
2023-05-09 22:13:59 +0530

    Added user edit profile functionality

--- base/views.py
+++ base/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
@@ -201,3 +201,19 @@ def deleteMessage(request, pk):
         'obj': message
     }
     return render(request, 'base/delete.html', context)
+
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+
+    context = {
+        'form': form
+    }
+    return render(request, 'base/update-user.html', context)
